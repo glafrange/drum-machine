798f60a0d8655aaa2ab591cd6dfeb0ae2b2bf261
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-07-10 23:26:52 -0400

    Added all sounds

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// document.querySelector("#kick").addEventListener("click", function(e) {
-//   e.target.querySelector("#Q").play();
-// });
-
 document.querySelectorAll(".drum-pad").forEach(drumPad => {
   drumPad.addEventListener("click", function(e) {
     e.target.querySelector("audio").play();
