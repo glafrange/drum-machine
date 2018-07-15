8be085ff63f2ebddb92628455ac7e38a214a3e32
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-07-14 22:03:54 -0400

    added keydown event listeners, clips now restart when pressed again

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 document.querySelectorAll(".drum-pad").forEach(drumPad => {
   drumPad.addEventListener("click", function(e) {
-    e.target.querySelector("audio").play();
+    e.target.closest(".drum-pad").querySelector("audio").play();
   });
+});
+
+const drumKeys = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c'];
+
+document.addEventListener("keydown", function(e) {
+  if(drumKeys.includes(e.key)) {
+    const clip = document.querySelector("#" + e.key.toUpperCase());
+    clip.pause();
+    clip.currentTime = 0;
+    clip.play();
+  }
 });
